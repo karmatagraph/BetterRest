82515a83eb3298a9d0176eb1d977ffff29b3f469
karma <karmatashi954@gmail.com>
2022-05-09 16:21:16 +0545

    add of more cleaner design

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,33 +10,56 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var wakeTime = Date.now
+    @State private var wakeTime = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var currentDate = Date.now
     @State private var coffeeAmount = 1
     @State private var sleep = false
     @State var sleepTime = Date.now
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
     var body: some View {
         NavigationView{
-            VStack{
-                Text("When do you want to wake up")
-                    .font(.headline)
-                DatePicker("Wake up time: ",selection: $wakeTime , displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                    .padding(40)
-                Text("How much sleep would you like to have")
-                    .font(.headline)
-                Stepper("Sleep amount: \(sleepAmount.formatted()) ", value: $sleepAmount, in: 4...12,step: 0.25)
-//                    .labelsHidden()
-                    .padding(40)
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 Cup" : "\(coffeeAmount) Cups", value: $coffeeAmount, in: 1...8)
-                    .padding(40)
+            Form{
+//                Spacer()
+                VStack(alignment: .leading, spacing: 0){
+                    Text("When do you want to wake up")
+                        .font(.headline)
+                    DatePicker("Wake up time: ",selection: $wakeTime , displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+//                        .padding(40)
+                }
+                .padding(10)
+//                Spacer()
+                VStack(alignment: .leading, spacing: 0){
+                    Text("How much sleep do you desire")
+                        .font(.headline)
+                    Stepper("Sleep amount: \(sleepAmount.formatted()) ", value: $sleepAmount, in: 4...12,step: 0.25)
+    //                    .labelsHidden()
+//                        .padding(40)
+                }
+                .padding(10)
+                
+//                Spacer()
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper(coffeeAmount == 1 ? "1 Cup" : "\(coffeeAmount) Cups", value: $coffeeAmount, in: 1...8)
+//                        .padding(40)
+                }
+                .padding(10)
+//                Spacer()
+                
                 
             }
             .navigationTitle("Bed Time")
+            .listSectionSeparator(.hidden)
             .toolbar{
                 Button(action:calculateBedTime){
                     Image(systemName: "bed.double.fill")
